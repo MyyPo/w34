9e4790827d30a3ad8bf49b82bbedd99f6b5267cc
Myypo <nikirsmcgl@gmail.com>
2022-12-17 11:31:25 +0200

    Refactored dev methods

--- internal/pkg/dev/dev.go
+++ internal/pkg/dev/dev.go
@@ -22,20 +22,14 @@ func (s DevServer) CreateProject(
 	ctx context.Context,
 	req *devv1.NewProjectRequest,
 ) (*devv1.NewProjectResponse, error) {
-
-	md, ok := metadata.FromIncomingContext(ctx)
-	if !ok {
-		return nil, fmt.Errorf("metadata was not provided")
+	reqUserID, err := getUserID(ctx)
+	if err != nil {
+		return nil, err
 	}
-	userIDArr := md["user_id"]
-	if len(userIDArr) == 0 {
-		return nil, fmt.Errorf("metadata was not provided")
-	}
-	userID := userIDArr[0]
 
 	projectName := req.GetName()
 
-	_, err := s.repo.CreateProject(ctx, projectName, userID)
+	_, err = s.repo.CreateProject(ctx, projectName, reqUserID)
 	if err != nil {
 		return nil, err
 	}
@@ -47,19 +41,14 @@ func (s DevServer) DeleteProject(
 	ctx context.Context,
 	req *devv1.DeleteProjectRequest,
 ) (*devv1.DeleteProjectResponse, error) {
-	md, ok := metadata.FromIncomingContext(ctx)
-	if !ok {
-		return nil, fmt.Errorf("metadata was not provided")
+	reqUserID, err := getUserID(ctx)
+	if err != nil {
+		return nil, err
 	}
-	userIDArr := md["user_id"]
-	if len(userIDArr) == 0 {
-		return nil, fmt.Errorf("metadata was not provided")
-	}
-	userID := userIDArr[0]
 
 	projectName := req.GetName()
 
-	err := s.repo.DeleteProject(ctx, projectName, userID)
+	err = s.repo.DeleteProject(ctx, projectName, reqUserID)
 	if err != nil {
 		return nil, err
 	}
@@ -71,23 +60,32 @@ func (s DevServer) CreateLocation(
 	ctx context.Context,
 	req *devv1.NewLocationRequest,
 ) (*devv1.NewLocationResponse, error) {
-	md, ok := metadata.FromIncomingContext(ctx)
-	if !ok {
-		return nil, fmt.Errorf("metadata was not provided")
-	}
-	userIDArr := md["user_id"]
-	if len(userIDArr) == 0 {
-		return nil, fmt.Errorf("metadata was not provided")
+	reqUserID, err := getUserID(ctx)
+	if err != nil {
+		return nil, err
 	}
 
-	userID := userIDArr[0]
 	projectName := req.GetProjectName()
 	locationName := req.GetLocationName()
 
-	_, err := s.repo.CreateLocation(ctx, projectName, locationName, userID)
+	_, err = s.repo.CreateLocation(ctx, projectName, locationName, reqUserID)
 	if err != nil {
 		return nil, err
 	}
 
 	return &devv1.NewLocationResponse{}, nil
+}
+
+func getUserID(ctx context.Context) (string, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return "", fmt.Errorf("metadata was not provided")
+	}
+	userIDArr := md["user_id"]
+	if len(userIDArr) == 0 {
+		return "", fmt.Errorf("metadata was not provided")
+	}
+	userID := userIDArr[0]
+
+	return userID, nil
 }
