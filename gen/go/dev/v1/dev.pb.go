078dd4758b3c89eb6fb07e8f98b244e51cad884b
Myypo <nikirsmcgl@gmail.com>
2022-12-19 20:48:06 +0200

    Added stub get location scenes proto

--- gen/go/dev/v1/dev.pb.go
+++ gen/go/dev/v1/dev.pb.go
@@ -401,6 +401,99 @@ func (x *NewSceneResponse) GetSceneId() int32 {
 	return 0
 }
 
+type GetLocationScenesRequest struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Project  string `protobuf:"bytes,1,opt,name=project,proto3" json:"project,omitempty"`
+	Location string `protobuf:"bytes,2,opt,name=location,proto3" json:"location,omitempty"`
+}
+
+func (x *GetLocationScenesRequest) Reset() {
+	*x = GetLocationScenesRequest{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_dev_v1_dev_proto_msgTypes[8]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *GetLocationScenesRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*GetLocationScenesRequest) ProtoMessage() {}
+
+func (x *GetLocationScenesRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_dev_v1_dev_proto_msgTypes[8]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use GetLocationScenesRequest.ProtoReflect.Descriptor instead.
+func (*GetLocationScenesRequest) Descriptor() ([]byte, []int) {
+	return file_dev_v1_dev_proto_rawDescGZIP(), []int{8}
+}
+
+func (x *GetLocationScenesRequest) GetProject() string {
+	if x != nil {
+		return x.Project
+	}
+	return ""
+}
+
+func (x *GetLocationScenesRequest) GetLocation() string {
+	if x != nil {
+		return x.Location
+	}
+	return ""
+}
+
+type GetLocationScenesResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+}
+
+func (x *GetLocationScenesResponse) Reset() {
+	*x = GetLocationScenesResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_dev_v1_dev_proto_msgTypes[9]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *GetLocationScenesResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*GetLocationScenesResponse) ProtoMessage() {}
+
+func (x *GetLocationScenesResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_dev_v1_dev_proto_msgTypes[9]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use GetLocationScenesResponse.ProtoReflect.Descriptor instead.
+func (*GetLocationScenesResponse) Descriptor() ([]byte, []int) {
+	return file_dev_v1_dev_proto_rawDescGZIP(), []int{9}
+}
+
 var File_dev_v1_dev_proto protoreflect.FileDescriptor
 
 var file_dev_v1_dev_proto_rawDesc = []byte{
@@ -438,14 +531,21 @@ var file_dev_v1_dev_proto_rawDesc = []byte{
 	0x01, 0x22, 0x2d, 0x0a, 0x10, 0x4e, 0x65, 0x77, 0x53, 0x63, 0x65, 0x6e, 0x65, 0x52, 0x65, 0x73,
 	0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x19, 0x0a, 0x08, 0x73, 0x63, 0x65, 0x6e, 0x65, 0x5f, 0x69,
 	0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x05, 0x52, 0x07, 0x73, 0x63, 0x65, 0x6e, 0x65, 0x49, 0x64,
-	0x42, 0x75, 0x0a, 0x0a, 0x63, 0x6f, 0x6d, 0x2e, 0x64, 0x65, 0x76, 0x2e, 0x76, 0x31, 0x42, 0x08,
-	0x44, 0x65, 0x76, 0x50, 0x72, 0x6f, 0x74, 0x6f, 0x50, 0x01, 0x5a, 0x24, 0x67, 0x69, 0x74, 0x68,
-	0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x4d, 0x79, 0x79, 0x50, 0x6f, 0x2f, 0x77, 0x33, 0x34,
-	0x2e, 0x47, 0x6f, 0x2f, 0x64, 0x65, 0x76, 0x2f, 0x76, 0x31, 0x3b, 0x64, 0x65, 0x76, 0x76, 0x31,
-	0xa2, 0x02, 0x03, 0x44, 0x58, 0x58, 0xaa, 0x02, 0x06, 0x44, 0x65, 0x76, 0x2e, 0x56, 0x31, 0xca,
-	0x02, 0x06, 0x44, 0x65, 0x76, 0x5c, 0x56, 0x31, 0xe2, 0x02, 0x12, 0x44, 0x65, 0x76, 0x5c, 0x56,
-	0x31, 0x5c, 0x47, 0x50, 0x42, 0x4d, 0x65, 0x74, 0x61, 0x64, 0x61, 0x74, 0x61, 0xea, 0x02, 0x07,
-	0x44, 0x65, 0x76, 0x3a, 0x3a, 0x56, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x22, 0x50, 0x0a, 0x18, 0x47, 0x65, 0x74, 0x4c, 0x6f, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x53,
+	0x63, 0x65, 0x6e, 0x65, 0x73, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x18, 0x0a, 0x07,
+	0x70, 0x72, 0x6f, 0x6a, 0x65, 0x63, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x70,
+	0x72, 0x6f, 0x6a, 0x65, 0x63, 0x74, 0x12, 0x1a, 0x0a, 0x08, 0x6c, 0x6f, 0x63, 0x61, 0x74, 0x69,
+	0x6f, 0x6e, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x6c, 0x6f, 0x63, 0x61, 0x74, 0x69,
+	0x6f, 0x6e, 0x22, 0x1b, 0x0a, 0x19, 0x47, 0x65, 0x74, 0x4c, 0x6f, 0x63, 0x61, 0x74, 0x69, 0x6f,
+	0x6e, 0x53, 0x63, 0x65, 0x6e, 0x65, 0x73, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x42,
+	0x75, 0x0a, 0x0a, 0x63, 0x6f, 0x6d, 0x2e, 0x64, 0x65, 0x76, 0x2e, 0x76, 0x31, 0x42, 0x08, 0x44,
+	0x65, 0x76, 0x50, 0x72, 0x6f, 0x74, 0x6f, 0x50, 0x01, 0x5a, 0x24, 0x67, 0x69, 0x74, 0x68, 0x75,
+	0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x4d, 0x79, 0x79, 0x50, 0x6f, 0x2f, 0x77, 0x33, 0x34, 0x2e,
+	0x47, 0x6f, 0x2f, 0x64, 0x65, 0x76, 0x2f, 0x76, 0x31, 0x3b, 0x64, 0x65, 0x76, 0x76, 0x31, 0xa2,
+	0x02, 0x03, 0x44, 0x58, 0x58, 0xaa, 0x02, 0x06, 0x44, 0x65, 0x76, 0x2e, 0x56, 0x31, 0xca, 0x02,
+	0x06, 0x44, 0x65, 0x76, 0x5c, 0x56, 0x31, 0xe2, 0x02, 0x12, 0x44, 0x65, 0x76, 0x5c, 0x56, 0x31,
+	0x5c, 0x47, 0x50, 0x42, 0x4d, 0x65, 0x74, 0x61, 0x64, 0x61, 0x74, 0x61, 0xea, 0x02, 0x07, 0x44,
+	0x65, 0x76, 0x3a, 0x3a, 0x56, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -460,25 +560,27 @@ func file_dev_v1_dev_proto_rawDescGZIP() []byte {
 	return file_dev_v1_dev_proto_rawDescData
 }
 
-var file_dev_v1_dev_proto_msgTypes = make([]protoimpl.MessageInfo, 9)
+var file_dev_v1_dev_proto_msgTypes = make([]protoimpl.MessageInfo, 11)
 var file_dev_v1_dev_proto_goTypes = []interface{}{
-	(*NewProjectRequest)(nil),     // 0: dev.v1.NewProjectRequest
-	(*NewProjectResponse)(nil),    // 1: dev.v1.NewProjectResponse
-	(*DeleteProjectRequest)(nil),  // 2: dev.v1.DeleteProjectRequest
-	(*DeleteProjectResponse)(nil), // 3: dev.v1.DeleteProjectResponse
-	(*NewLocationRequest)(nil),    // 4: dev.v1.NewLocationRequest
-	(*NewLocationResponse)(nil),   // 5: dev.v1.NewLocationResponse
-	(*NewSceneRequest)(nil),       // 6: dev.v1.NewSceneRequest
-	(*NewSceneResponse)(nil),      // 7: dev.v1.NewSceneResponse
-	nil,                           // 8: dev.v1.NewSceneRequest.OptionsEntry
+	(*NewProjectRequest)(nil),         // 0: dev.v1.NewProjectRequest
+	(*NewProjectResponse)(nil),        // 1: dev.v1.NewProjectResponse
+	(*DeleteProjectRequest)(nil),      // 2: dev.v1.DeleteProjectRequest
+	(*DeleteProjectResponse)(nil),     // 3: dev.v1.DeleteProjectResponse
+	(*NewLocationRequest)(nil),        // 4: dev.v1.NewLocationRequest
+	(*NewLocationResponse)(nil),       // 5: dev.v1.NewLocationResponse
+	(*NewSceneRequest)(nil),           // 6: dev.v1.NewSceneRequest
+	(*NewSceneResponse)(nil),          // 7: dev.v1.NewSceneResponse
+	(*GetLocationScenesRequest)(nil),  // 8: dev.v1.GetLocationScenesRequest
+	(*GetLocationScenesResponse)(nil), // 9: dev.v1.GetLocationScenesResponse
+	nil,                               // 10: dev.v1.NewSceneRequest.OptionsEntry
 }
 var file_dev_v1_dev_proto_depIdxs = []int32{
-	8, // 0: dev.v1.NewSceneRequest.options:type_name -> dev.v1.NewSceneRequest.OptionsEntry
-	1, // [1:1] is the sub-list for method output_type
-	1, // [1:1] is the sub-list for method input_type
-	1, // [1:1] is the sub-list for extension type_name
-	1, // [1:1] is the sub-list for extension extendee
-	0, // [0:1] is the sub-list for field type_name
+	10, // 0: dev.v1.NewSceneRequest.options:type_name -> dev.v1.NewSceneRequest.OptionsEntry
+	1,  // [1:1] is the sub-list for method output_type
+	1,  // [1:1] is the sub-list for method input_type
+	1,  // [1:1] is the sub-list for extension type_name
+	1,  // [1:1] is the sub-list for extension extendee
+	0,  // [0:1] is the sub-list for field type_name
 }
 
 func init() { file_dev_v1_dev_proto_init() }
@@ -583,6 +685,30 @@ func file_dev_v1_dev_proto_init() {
 				return nil
 			}
 		}
+		file_dev_v1_dev_proto_msgTypes[8].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*GetLocationScenesRequest); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_dev_v1_dev_proto_msgTypes[9].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*GetLocationScenesResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -590,7 +716,7 @@ func file_dev_v1_dev_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_dev_v1_dev_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   9,
+			NumMessages:   11,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
--- gen/go/dev/v1/dev_service_grpc.pb.go
+++ gen/go/dev/v1/dev_service_grpc.pb.go
@@ -32,6 +32,8 @@ type DevServiceClient interface {
 	// Scene is the basic unit of the developed game
 	// Containing player options and possibly encounters
 	NewScene(ctx context.Context, in *NewSceneRequest, opts ...grpc.CallOption) (*NewSceneResponse, error)
+	// Returns all scenes linked to a certain location
+	GetLocationScenes(ctx context.Context, in *GetLocationScenesRequest, opts ...grpc.CallOption) (*GetLocationScenesResponse, error)
 }
 
 type devServiceClient struct {
@@ -78,6 +80,15 @@ func (c *devServiceClient) NewScene(ctx context.Context, in *NewSceneRequest, op
 	return out, nil
 }
 
+func (c *devServiceClient) GetLocationScenes(ctx context.Context, in *GetLocationScenesRequest, opts ...grpc.CallOption) (*GetLocationScenesResponse, error) {
+	out := new(GetLocationScenesResponse)
+	err := c.cc.Invoke(ctx, "/dev.v1.DevService/GetLocationScenes", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // DevServiceServer is the server API for DevService service.
 // All implementations must embed UnimplementedDevServiceServer
 // for forward compatibility
@@ -92,6 +103,8 @@ type DevServiceServer interface {
 	// Scene is the basic unit of the developed game
 	// Containing player options and possibly encounters
 	NewScene(context.Context, *NewSceneRequest) (*NewSceneResponse, error)
+	// Returns all scenes linked to a certain location
+	GetLocationScenes(context.Context, *GetLocationScenesRequest) (*GetLocationScenesResponse, error)
 	mustEmbedUnimplementedDevServiceServer()
 }
 
@@ -110,6 +123,9 @@ func (UnimplementedDevServiceServer) NewLocation(context.Context, *NewLocationRe
 }
 func (UnimplementedDevServiceServer) NewScene(context.Context, *NewSceneRequest) (*NewSceneResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method NewScene not implemented")
+}
+func (UnimplementedDevServiceServer) GetLocationScenes(context.Context, *GetLocationScenesRequest) (*GetLocationScenesResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetLocationScenes not implemented")
 }
 func (UnimplementedDevServiceServer) mustEmbedUnimplementedDevServiceServer() {}
 
@@ -196,6 +212,24 @@ func _DevService_NewScene_Handler(srv interface{}, ctx context.Context, dec func
 	return interceptor(ctx, in, info, handler)
 }
 
+func _DevService_GetLocationScenes_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetLocationScenesRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(DevServiceServer).GetLocationScenes(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/dev.v1.DevService/GetLocationScenes",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(DevServiceServer).GetLocationScenes(ctx, req.(*GetLocationScenesRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // DevService_ServiceDesc is the grpc.ServiceDesc for DevService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -218,6 +252,10 @@ var DevService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "NewScene",
 			Handler:    _DevService_NewScene_Handler,
+		},
+		{
+			MethodName: "GetLocationScenes",
+			Handler:    _DevService_GetLocationScenes_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
